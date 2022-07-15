eb9c5a0df1fe27c8e1916f504b0bd648f8880095
nitin chandrakar <nitin.it90@gmail.com>
2022-07-15 20:46:47 +0530

    foreach polyfill

---
+++ Polyfills/foreach-polyfills.js
@@ -0,0 +1,16 @@
+var arr = [1, 2, 3, 7, 4, 5, 6];
+
+arr.forEach(function (value, index, array) {
+  console.log(value);
+});
+
+Array.prototype.forEachPoly = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
+};
+
+console.log("---------");
+arr.forEachPoly(function (value, index, array) {
+  console.log(value);
+});
